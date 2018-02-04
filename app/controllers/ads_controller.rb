4491f2f9d42861e51da8d2f991f91be47a6e5d6c
Casagrande <casagrande@Host-004.home>
2018-02-04 00:05:04 -0200

    adicionado função para criar anuncio atravez da url /ads/new

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -4,11 +4,20 @@ class AdsController < ApplicationController
     @ad = Ad.new
   end
 
+  def create 
+    @ad = Ad.new(ads_params)
+    @ad.save
+  end
+
   def show
     @ad = Ad.find(params[:id])
   end
 
   def index
     @ads = Ad.all
+  end
+
+  def ads_params
+    params.require(:ad).permit(:name, :description, :price, :seller_id, :email, :img_url)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   get '/ads/new', :controller => 'ads', :action=> 'new'
-  get '/ads/create', :controller => 'ads', :action=> 'create'
+  post '/ads/create', :controller=> 'ads', :action=> 'create'
   get '/ads/', :controller => 'ads', :action=> 'index'
   get '/ads/:id', :controller=> 'ads', :action=>'show'
 
